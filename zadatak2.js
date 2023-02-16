d241f6f983a25b48e02ffd0276b24e0c0f4adf7d
jtustanic <josipa.tustanic@gmail.com>
2023-02-16 19:03:21 +0100

    -

---
+++ zadatak2.js
@@ -0,0 +1,20 @@
+var text = 'Dodajem nekakav tekst eto tak malo      ';
+
+var length = text.length;
+//console.log("duljina teksta = " + length);
+
+var wordSit = text.substring(19,3);
+//console.log(wordSit);
+
+
+text = text.replace('tak', 'molim');
+
+var newText = text + ', jos bih ja tu malo teksta';
+//console.log(newText);
+
+text = text.toLocaleLowerCase();
+console.log(text);
+
+text = text.trim();
+//console.log(text);
+
